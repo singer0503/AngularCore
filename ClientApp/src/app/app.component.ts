b96d2803d5e69e5322f0bd02c6ba36ef74bb7abf
max <singer0503@gmail.com>
2019-03-25 15:44:48 +0800

    增加sass套件

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { MatDialog, MatDialogRef, MatSnackBar } from '@angular/material';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   isDarkTheme = false;
